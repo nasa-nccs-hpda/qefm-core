e2a54384b1502a3b5bb42e1407ddf521f89265f5
gtamkin <glenn.s.tamkin@nasa.gov>
2025-01-03 21:31:49 -0500

    fm_graphcast - suppress gcp bucket access

--- tests/fm-fourcastnet.sh
+++ tests/fm-fourcastnet.sh
@@ -1,6 +1,10 @@
 echo "FourCastNet":
-cd /discover/nobackup/projects/QEFM/qefm-core/qefm/models/src/FMFourCastNet/FourCastNet
 module load singularity
-cmd="time singularity exec --nv -B /discover/nobackup/projects/QEFM/qefm-core/qefm /discover/nobackup/projects/QEFM/containers/"$1" python -u -m torch.distributed.run /discover/nobackup/projects/QEFM/qefm-core/qefm/models/src/FMFourCastNet/FourCastNet/inference/inference.py --config=afno_backbone --run_num=0 --weights /discover/nobackup/projects/QEFM/qefm-core/qefm/models/checkpoints/FMFourCastNet/ccai_demo/model_weights/FCN_weights_v0/backbone.ckpt --override ./out/20241229_0917 --vis"
+cd /discover/nobackup/projects/QEFM/qefm-core/qefm/models/src/FMFourCastNet/FourCastNet
+echo 'cd /discover/nobackup/projects/QEFM/qefm-core/qefm/models/src/FMFourCastNet/FourCastNet'
+
+#cd /discover/nobackup/projects/QEFM/qefm-core
+#echo 'cd /discover/nobackup/projects/QEFM/qefm-core'
+cmd="time singularity exec --nv -B /discover/nobackup/projects/QEFM/qefm-core /discover/nobackup/projects/QEFM/containers/"$1" python -u -m torch.distributed.run /discover/nobackup/projects/QEFM/qefm-core/qefm/models/src/FMFourCastNet/FourCastNet/inference/inference.py --config=afno_backbone --run_num=0 --weights /discover/nobackup/projects/QEFM/qefm-core/qefm/models/checkpoints/FMFourCastNet/ccai_demo/model_weights/FCN_weights_v0/backbone.ckpt --override ./out/20240103_1555 --vis"
 echo $cmd
 $cmd
