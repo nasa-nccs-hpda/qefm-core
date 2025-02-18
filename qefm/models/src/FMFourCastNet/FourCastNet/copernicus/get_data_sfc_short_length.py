d678de6943fbd4c597eb15156706db1566c06c01
[jli30] <[jain.li@nasa.gov]>
2025-02-18 09:57:38 -0500

    pre-process for AFNO

--- qefm/models/src/FMFourCastNet/FourCastNet/copernicus/get_data_sfc_short_length.py
+++ qefm/models/src/FMFourCastNet/FourCastNet/copernicus/get_data_sfc_short_length.py
@@ -11,21 +11,17 @@ c.retrieve(
             '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_temperature',
             'mean_sea_level_pressure', 'surface_pressure', 'total_column_water_vapour',
         ],
-        'year': '2021',
-        'month': '10',
+        'year': '2025',
+        'month': '01',
         'day': [
-            '19', '20', '21',
-            '22', '23', '24',
-            '25', '26', '27',
-            '28', '29', '30',
-            '31',
+            '01', '02', '03',
+            '04', '05',
         ],
         'time': [
             '00:00', '06:00', '12:00',
             '18:00',
         ],
     },
-    '/project/projectdirs/dasrepo/ERA5/oct_2021_19_31_sfc.nc')
+    '/discover/nobackup/jli30/data/FNO/ERA5/jan_2025_01_05_sfc.nc')
 
 
-#    '/project/projectdirs/dasrepo/ERA5/oct_2021_19_31_pl.nc')
