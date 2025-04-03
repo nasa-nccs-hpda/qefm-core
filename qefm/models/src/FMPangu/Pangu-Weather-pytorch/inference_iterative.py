313cb9113ff1cc926fbaf4432c11ef6f0ac48dfd
JianLi <44811760+jli-99@users.noreply.github.com>
2025-04-03 12:25:57 -0400

    fix dim level name

--- qefm/models/src/FMPangu/Pangu-Weather-pytorch/inference_iterative.py
+++ qefm/models/src/FMPangu/Pangu-Weather-pytorch/inference_iterative.py
@@ -23,7 +23,7 @@ def load_input_surface(input_data_dir: str, input_file: str, tidx:int = 0) -> np
     ds = xr.open_dataset(data_root / input_file)
     variables = ['msl', 'u10', 'v10', 't2m']
     levels = [1000, 925, 850, 700, 600, 500, 400, 300, 250, 200, 150, 100, 50]
-    data = np.stack([ds[var].isel(valid_time=tidx).sel(levels, dims='level').values for var in variables], axis=0)
+    data = np.stack([ds[var].isel(valid_time=tidx).sel(levels, dims='pressure_level').values for var in variables], axis=0)
     return data
 
 def pred_to_ds(surface: np.ndarray, atmos: np.ndarray, time_value: np.datetime64) -> xr.Dataset:
