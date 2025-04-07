5e57ba6eead6024302c18218e9ca5c5e37153163
JianLi <44811760+jli-99@users.noreply.github.com>
2025-04-07 16:25:39 -0400

    fill lat=90

--- data/prithvi_cf.py
+++ data/prithvi_cf.py
@@ -267,9 +267,7 @@ for file in files:
 
         # using zonal mean to replace lat=90
         zmean = ds_new[var].sel(lat=89.5).mean(dim="lon")
-        ds_new[var].loc[dict(lat=90.0)] = zmean.expand_dims(dim={"lat": 1})\
-            .broadcast_like(ds_new[var].sel(lat=90.0))
-
+        ds_new[var].loc[dict(lat=90.0)] = zmean.expand_dims(dim={"lon": ds.lon})
 
     #     arr = ds[var].values
     #     if len(arr.shape) == 3:
