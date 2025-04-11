0dad083ae10250d20abe063ab160114e0d238363
JianLi <44811760+jli-99@users.noreply.github.com>
2025-04-10 21:31:22 -0400

    convert initial condition of aifs

---
+++ data/aifs_cf_init.py
@@ -0,0 +1,256 @@
+import xarray as xr
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser(description="Convert AIFS output to CF-compliant NetCDF")
+# parser.add_argument("input_dir", type=str, help="Path to GenCast output directory")
+# parser.add_argument("fmodel", type=str, help="Model name")
+parser.add_argument("--year", "-y", type=str, help="Year")
+parser.add_argument("--month", "-m", type=str, help="Month")
+parser.add_argument("--day", "-d", type=str, help="Day")
+args = parser.parse_args()
+
+#input_dir = Path("/discover/nobackup/projects/QEFM/data/")
+input_dir = Path("/discover/nobackup/projects/QEFM/data/FMAifs/nc_files")
+fmodel = "FMAifs"
+yyyy = args.year
+mm = args.month
+dd = args.day
+
+files = input_dir.glob(f"aifs-dataset-source-era5_date-{yyyy}-{mm}-{dd}*.nc")
+# file_path = input_dir / "pressure_hourly" / "inst" / f"Y{yyyy}" / f"M{mm}" 
+# files.append(sorted(file_path.glob(f"era5_atmos-inst_allvar_{yyyy}{mm}{dd}_00z.nc"))[0])
+# file_path = input_dir / "surface_hourly" / "inst" / f"Y{yyyy}" / f"M{mm}" 
+# files.append(sorted(file_path.glob(f"era5_surface-inst_allvar_{yyyy}{mm}{dd}_00z.nc"))[0])
+
+
+
+ds = xr.open_dataset(files[0])
+
+ds = ds.rename({'level': 'lev', 'latitude': 'lat', 'longitude': 'lon'})
+ds = ds.isel(time=1).expand_dims('time')
+
+MAPL_GRAV = np.float32(9.80665)
+FILL_VALUE = np.float32(1.e+15)
+dt = pd.to_datetime(f"{yyyy}-{mm}-{dd} 00:00:00")
+
+print("Processing file : ", files)
+print("At Open : \n", ds)
+
+## Coordinates
+# Time
+HH = dt.strftime("%H")
+YYYY = dt.strftime("%Y")
+MM = dt.strftime("%m")
+DD = dt.strftime("%d")
+long_name = "time"
+begin_date = np.int32(f"{YYYY}{MM}{DD}")
+begin_time = np.int32(dt.hour*10000)
+time_increment = np.int32(60000)
+
+units = f"hours since {YYYY}-{MM}-{DD} {HH}:00:00"
+calendar = "proleptic_gregorian"
+
+# get time stamp for output file
+#t = ds['time'].values
+tstamp = dt.strftime("%Y-%m-%dT%H") 
+print("Time stamp : ", tstamp)
+
+
+# change value of time
+#ref_time = np.datetime64("2024-12-01T00:00:00", "ns")
+#ds['time'] = np.float32((ds['time']-ref_time)/np.timedelta64(1, 'h'))
+ds['time'] = np.float32((ds['time']-ds['time'])/np.timedelta64(1, 'h'))
+#ds['time'].values = np.float32(0.0)
+# add attributes
+ds.time.attrs = {
+    "long_name" : long_name,
+    "units" : units,
+    "calendar" : calendar,
+    "begin_date" : begin_date,
+    "begin_time" : begin_time,
+    "time_increment": time_increment
+}
+
+# Latitude
+lats = ds['lat'].values.astype(np.float32)
+ds['lat'] = lats
+
+if lats[0] > lats[-1]:
+    # Flip the data array
+    ds = ds.sel(lat=slice(None, None, -1))
+ds.lat.attrs = {
+    "long_name" : "latitude",
+    "units" : "degrees_north",
+}
+
+# Longitude
+lons = ds['lon'].values.astype(np.float32)
+ds['lon'] = lons
+if min(lons) == 0:
+    ds['lon'] = ((ds["lon"] + 180) % 360) - 180
+    ds = ds.sortby(ds.lon)
+ds.lon.attrs = {
+    "long_name" : "longitude",
+    "units" : "degrees_east",
+}
+
+# level
+#ds = ds.rename({'level': 'lev'})
+levs = ds['lev'].values.astype(np.float32)
+ds['lev'] = levs
+if levs[0] < levs[-1]:
+    # Flip the level array
+    ds['lev'] = levs[::-1]
+    # Flip the data array
+    ds = ds.sel(lev=slice(None, None, -1))
+ds.lev.attrs = {
+    "long_name" : "pressure_level",
+    "units" : "hPa",
+}
+print("After coord \n", ds)
+
+## Variables
+# rename variables
+rename_dict = {
+    "10m_u_component_of_wind": "U10M",
+    "10m_v_component_of_wind": "V10M",
+    "2m_temperature": "T2M",
+    "2m_dewpoint_temperature": "T2MDEW",
+    "mean_sea_level_pressure": "SLP",
+    "skin_temperature": "TS",
+    "surface_pressure" : "PS",
+    "total_column_water" : "TQL",
+    "geopotential_at_surface" : "PHIS",
+    "geopotential": "H",
+    "specific_humidity": "QV",
+    "temperature": "T",
+    "u_component_of_wind": "U",
+    "v_component_of_wind": "V",
+    "vertical_velocity": "OMEGA",
+}
+# Subset the dataset to include only the variables of interest
+ds = ds[list(rename_dict.keys())]
+# Rename the variables
+ds = ds.rename(rename_dict)
+print("After rename \n ", ds)
+
+
+
+# map attributes
+varMap = {
+    "U10M": {
+        "long_name" : "10-meter_eastward_wind",
+        "units" : "m s-1",
+    },
+    "V10M": {
+        "long_name" : "10-meter_northward_wind",
+        "units" : "m s-1",
+    },
+    "T2M": {
+        "long_name" : "2-meter_air_temperature",
+        "units" : "K",
+    },
+    "T2MDEW": {
+        "long_name" : "dew_point_temperature_at_2_m",
+        "units" : "K",
+    },
+    "TS": {
+        "long_name" : "surface_skin_temperature",
+        "units" : "K",
+    },
+    "SLP": {
+        "long_name" : "surface_air_pressure",
+        "units" : "Pa",
+    },
+    "PS": { 
+        "long_name" : "surface_pressure",
+        "units" : "Pa",
+    },
+    "TQL": {
+        "long_name" : "total_precipitable_liquid_water",
+        "units" : "kg m-2",
+    },
+    "H": {
+        "long_name" : "height",
+        "units" : "m",
+    },
+    "QV": {
+        "long_name" : "specific_humidity",
+        "units" : "kg kg-1",
+    },
+    "T": {
+        "long_name" : "air_temperature",
+        "units" : "K",
+    },
+    "U": {
+        "long_name" : "eastward_wind",
+        "units" : "m s-1",
+    },
+    "V": {
+        "long_name" : "northward_wind",
+        "units" : "m s-1",
+    },
+    "OMEGA": {
+        "long_name" : "vertical_pressure_velocity",
+        "units" : "Pa s-1",
+    },
+    "PHIS": {
+        "long_name" : "surface_geopotential_height",
+        "units" : "m+2 s-2",
+    },
+}
+
+# define chunk size for each variable
+
+## TODO : Need to check surface geopotential height from ERA5
+## Get geopotential height 
+ds['H'] = ds['H']/MAPL_GRAV
+
+## mask variables based on elevation
+# topo
+topo = ds.PHIS.values/MAPL_GRAV
+height = ds.H.values
+mask = np.where(height > topo, 1, 0)
+for var in ds.data_vars:
+    # add attributes
+    ds[var].attrs = varMap[var]
+    ds[var].attrs['_FillValue'] = FILL_VALUE
+    ds[var].attrs['missing_value'] = FILL_VALUE
+    ds[var].attrs['fmissing_value'] = FILL_VALUE
+    # mask 3d variables
+    if 'lev' in ds[var].dims:
+        ds[var] = ds[var].where(mask == 1, FILL_VALUE)
+# chunk 
+# nlats = len(ds.lat)
+# nlons = len(ds.lon)
+# chunks_size = {"ens": 1, "time": 1, "lev": 1, "lat": nlats, "lon": nlons}
+# ds = ds.chunk(chunks_size)
+print("After variable \n", ds)
+
+ds = ds.drop_vars(['datetime'])
+## add global attributes
+ds.attrs = {
+    "title" : f"{fmodel} initial input at {YYYY}-{MM}-{DD}T{HH}:00:00", 
+    "institution" : "NASA CISTO Data Science Group",
+    #"source" : f"{fmodel} model output",
+    "Conventions" : "CF",
+    "Comment" : "NetCDF-4" 
+}
+
+## Write to NetCDF
+compression = {"zlib": True, 
+            "complevel": 1,
+            "shuffle": True,}
+encoding = {var: compression for var in ds.data_vars}
+output_dir = Path(f"/discover/nobackup/projects/QEFM/data/rollout_outputs/{fmodel}/Y{yyyy}/M{mm}/D{dd}")
+output_dir.mkdir(parents=True, exist_ok=True)
+fname = f"{fmodel}-initial-era5_date-{tstamp}_res-0.25_levels-13.nc"
+output_file = output_dir / fname
+ds.to_netcdf(output_file, encoding=encoding, engine="netcdf4")
+
+
+
+print("Finished \n", ds)
