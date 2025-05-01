fe83bfff64413a65b305e923aa94745181530f5c
JianLi <44811760+jli-99@users.noreply.github.com>
2025-04-30 21:11:48 -0400

    rollout for year 2024

--- qefm/models/src/FMGenCast/graphcast/fm_gencast.py
+++ qefm/models/src/FMGenCast/graphcast/fm_gencast.py
@@ -177,7 +177,8 @@ def data_valid_for_model(file_name: str, params_file_name: str):
 #dataset_file_value = "/discover/nobackup/jli30/QEFM/qefm-core/data/gencast-dataset-source-era5_date-2024-12-01_res-1.0_levels-13_steps-01.nc"
 #dataset_file_value = "/discover/nobackup/jli30/QEFM/qefm-core/data/gencast-dataset-source-era5_date-2024-12-01_res-1.0_levels-13_steps-10.nc"
 #dataset_dir = "/discover/nobackup/projects/QEFM/data/FMGenCast"
-dataset_dir = "/discover/nobackup/projects/QEFM/data/FMGenCast/12hr/samples"
+#dataset_dir = "/discover/nobackup/projects/QEFM/data/FMGenCast/12hr/samples"
+dataset_dir = "/discover/nobackup/projects/QEFM/data/FMGenCast/12hr/Y2024"
 dataset_file_value = f"gencast-dataset-source-era5_date-{date_str}_res-1.0_levels-13_steps-20.nc"
 dataset_file = os.path.join(dataset_dir, dataset_file_value)
 print("dataset_file_value:\n", dataset_file_value, "\n")
@@ -338,7 +339,7 @@ for chunk in rollout.chunked_prediction_generator_multiple_runs(
     ):
     chunks.append(chunk)
 predictions = xarray.combine_by_coords(chunks)
-out_dir = "/discover/nobackup/projects/QEFM/data/rollout_outputs/FMGenCast/raw/geos"
+out_dir = "/discover/nobackup/projects/QEFM/data/rollout_outputs/FMGenCast/raw/Y2024"
 out_file_value = f"gencast-prediction-era5_date-{date_str}_res-1.0_levels-13_steps-20.nc"
 out_file = os.path.join(out_dir, out_file_value)
 predictions.to_netcdf(out_file)
