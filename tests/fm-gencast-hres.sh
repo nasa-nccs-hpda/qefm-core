b9b7ba261daa1e0eeb5f0b50ffbf99238489acc4
gtamkin <J3rgensen9!>
2025-05-17 15:56:07 -0400

    gencast hres

---
+++ tests/fm-gencast-hres.sh
@@ -0,0 +1,15 @@
+fm="GenCastHiRes"
+container="$1"/../containers/"$2"
+
+cd "$1"/qefm/models/src/FMGenCast
+current_dir=$(pwd)
+export PYTHONPATH="$1"/qefm/models/src/FMGenCast/graphcast
+if [[ ! -z "${PYTHONPATH}" ]]; then
+    echo "PYTHONPATH: "$PYTHONPATH""
+fi
+
+module load singularity
+cmd="time singularity exec --nv -B "$1"/qefm  "$1"/../containers/"$2" python "$1"/qefm/models/src/FMGenCast/fm_gencast_hres.py"
+echo $fm: $cmd
+$cmd
+
