845660fd1cc66d819511c228fe91e45081347f4d
gtamkin <J3rgensen9!>
2025-06-09 09:25:37 -0400

    fm-aifs.sh

---
+++ tests/fm-aifs.sh
@@ -0,0 +1,13 @@
+
+fm="AIFS"
+container="$1"/../containers/"$2"
+cd "$1"/qefm/models/src/FMAifs
+current_dir=$(pwd)
+if [[ ! -z "${PYTHONPATH}" ]]; then
+    echo "PYTHONPATH: "$PYTHONPATH""
+fi
+
+module load singularity
+cmd="time singularity exec --nv -B "$1"/qefm  "$1"/../containers/"$2" python -u -m torch.distributed.run "$1"/qefm/models/src/FMAifs/aifs-gpu-inference.py"
+echo $fm: $cmd
+$cmd
