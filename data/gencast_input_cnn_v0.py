59a7a9c0819209873c8f34be69cc67c9580c1812
JianLi <44811760+jli-99@users.noreply.github.com>
2025-06-12 10:24:48 -0400

    bug

--- data/gencast_input_cnn_v0.py
+++ data/gencast_input_cnn_v0.py
@@ -107,7 +107,7 @@ pred_vars = [var for var in ds.data_vars if var.endswith("_pred")]
 ds = ds[pred_vars]
 
 # subset the vertical levels
-ds = ds.sel(level=levs)
+ds = ds.sel(lev=levs)
 
 
 # ds_3d = xr.concat([xr.open_dataset(fp) for fp in fs[2:]], dim='time')
