2f4de894ea9a5c28eb7eb0886799d907baab820f
JianLi <44811760+jli-99@users.noreply.github.com>
2025-09-06 17:18:04 -0400

    get grads

--- qefm/models/src/FMGraphCast/fm_graphcast_ft.py
+++ qefm/models/src/FMGraphCast/fm_graphcast_ft.py
@@ -230,7 +230,7 @@ for epoch in range(num_epochs):
       # loss, diagnostics, state, grads = grads_fn_jitted(
       #     params, model_config, task_config, 
       #     state, batched_inputs, batched_targets, batched_forcings)
-      loss, diagnostics = loss_fn_jitted(
+      loss, diagnostics, next_state, grads = grads_fn_jitted(
          inputs=batched_inputs, targets=batched_targets, forcings=batched_forcings
       )
       print("Loss:", float(loss))
